64135e11753d3e83c8b6f706bf2aae7a2e67dee2
Matt Lanza <mlanza@amazon.com>
2026-03-17 15:47:24 -0400

    chore: remove compiled files from tracking

--- s3mrap-crr/lib/bootstrap-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-export interface BootstrapStackProps extends cdk.StackProps {
-    readonly project: string;
-    readonly primaryRegion: string;
-    readonly secondaryRegion: string;
-}
-export declare class BootstrapStack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props: BootstrapStackProps);
-}
