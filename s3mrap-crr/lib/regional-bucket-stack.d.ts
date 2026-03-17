00e9759289b28aa373d391034a62e680a5b0dc30
Matt Lanza <mlanza@amazon.com>
2026-03-17 15:47:24 -0400

    chore: remove compiled files from tracking

--- s3mrap-crr/lib/regional-bucket-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-export interface RegionalBucketStackProps extends cdk.StackProps {
-    readonly project: string;
-}
-export declare class RegionalBucketStack extends cdk.Stack {
-    readonly bucket: s3.Bucket;
-    constructor(scope: cdk.App, id: string, props: RegionalBucketStackProps);
-}
